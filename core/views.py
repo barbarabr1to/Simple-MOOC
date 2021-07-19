9f0c0f91f8d52c45cb4d3fb9f19e459222b04c81
Barbara Brito <barbarabritosz@hotmail.com>
2021-07-19 20:12:12 -0300

    Página de contato

--- core/views.py
+++ core/views.py
@@ -9,4 +9,7 @@ from django.http import HttpResponse
 # A função recebe obrigatoriamente o request que é a requisição atual
 def home(request):
     # 1°: requisição / 2°: o template a ser exibido / 3°: variáveis que ficam disponíveis no template
-    return render(request, 'home.html', {'usuario': 'Bárbara Brito'})
+    return render(request, 'home.html')
+
+def contact(request):
+    return render(request, 'contact.html')
